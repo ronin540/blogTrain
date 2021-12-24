91caf2bb20c04e06d3eadf7aacdc73f052f1048f
ronin540 <satyamor2@gmail.com>
2021-12-24 10:43:11 +0530

    db cofig changed

--- dbConfig/index.js
+++ dbConfig/index.js
@@ -1,21 +1,23 @@
 const mysql = require("mysql");
 
 function query(query, attributes) {
-	const connection = mysql.createConnection({
-		host: "bok3mkxfjk4xd2qijmji-mysql.services.clever-cloud.com",
-		user: "u9rqnmom5iykwybh",
-		password: "YtljYuv1bhEqht39pslj",
-		database: "bok3mkxfjk4xd2qijmji",
-	});
+	return new Promise((resolve, reject) => {
+		const connection = mysql.createConnection({
+			host: "bok3mkxfjk4xd2qijmji-mysql.services.clever-cloud.com",
+			user: "u9rqnmom5iykwybh",
+			password: "YtljYuv1bhEqht39pslj",
+			database: "bok3mkxfjk4xd2qijmji",
+		});
 
-	connection.query(query, attributes, function (error, results, fields) {
-		if (error) throw error;
-		return results;
-	});
-
-	connection.end((err) => {
-		if (err) throw err;
-		console.log("Connection is ended");
+		connection.query(query, attributes, (err, rows, fields) => {
+			if (err) {
+				return reject(err);
+			}
+			connection.end(() => {
+				console.log("con ended");
+			});
+			resolve(rows);
+		});
 	});
 }
 
